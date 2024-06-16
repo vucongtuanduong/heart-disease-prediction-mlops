432151822e9e7a684879fbfedfdecfab8e84fd62
Cong Tuan Duong Vu <117791793+vucongtuanduong@users.noreply.github.com>
2024-06-16 04:00:13 +0000

    add files

--- orchestration/heart-disease-prediction/data_loaders/ingest.py
+++ orchestration/heart-disease-prediction/data_loaders/ingest.py
@@ -3,18 +3,34 @@ if 'data_loader' not in globals():
 if 'test' not in globals():
     from mage_ai.data_preparation.decorators import test
 
-
+import pandas as pd 
+import requests
+from io import BytesIO
+from typing import List
 @data_loader
-def load_data(*args, **kwargs):
-    """
-    Template code for loading data from any source.
+# def load_data(*args, **kwargs):
+#     """
+#     Template code for loading data from any source.
 
-    Returns:
-        Anything (e.g. data frame, dictionary, array, int, str, etc.)
-    """
-    # Specify your data loading logic here
+#     Returns:
+#         Anything (e.g. data frame, dictionary, array, int, str, etc.)
+#     """
+#     # Specify your data loading logic here
+#     data = pd.read_csv('https://github.com/vucongtuanduong/heart-disease-prediction-mlops/blob/main/data/data.csv')
+#     return {data}
+def ingest_files(**kwargs) -> pd.DataFrame:
+    dfs: List[pd.DataFrame] = []
+    response = requests.get(
+        'https://github.com/vucongtuanduong/heart-disease-prediction-mlops/blob/main/data/data.csv'
+    )
 
-    return {}
+    if response.status_code != 200:
+        raise Exception(response.text)
+
+    df = pd.read_csv(BytesIO(response.content))
+    dfs.append(df)
+
+    return pd.concat(dfs)
 
 
 @test
