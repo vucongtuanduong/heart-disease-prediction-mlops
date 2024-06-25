2506a9c33ee0fa7abdd63971ca7392f923abcf6b
tuanduongvu <vucongtuanduong249@gmail.com>
2024-06-25 11:29:26 +0000

    add files

--- best-practices/code/integration_test.sh
+++ best-practices/code/integration_test.sh
@@ -6,10 +6,10 @@ sleep 10
 export S3_ENDPOINT_URL='http://localhost:4566'
 export INPUT_FILE_PATTERN='s3://heart-disease-prediction/in/test{prefix}.csv'
 export OUTPUT_FILE_PATTERN='s3://heart-disease-prediction/out/test{prefix}.csv'
-aws configure set aws_access_key_id test
-aws configure set aws_secret_access_key test
-aws configure set default.region us-east-1
-aws --endpoint-url="${S3_ENDPOINT_URL}" s3 mb s3://heart-disease-prediction
+pipenv run aws configure set aws_access_key_id test
+pipenv run aws configure set aws_secret_access_key test
+pipenv run aws configure set default.region us-east-1
+pipenv run aws --endpoint-url="${S3_ENDPOINT_URL}" s3 mb s3://heart-disease-prediction
 
 pipenv run python create_data_integration_test.py
 pipenv run python integration_test.py
